ea375bed4a21a2eb1bdc3c795cb7ba606257233e
TommasoBernardo <tommasob1210@gmail.com>
2023-01-16 16:06:22 +0100

    function for click button start search the movie (API)

--- src/components/ComponentsHeader.vue
+++ src/components/ComponentsHeader.vue
@@ -1,5 +1,6 @@
 
 <script>
+import axios from 'axios';
 import { store } from '../store.js'
 
 export default {
@@ -8,25 +9,35 @@ export default {
     data(){
         return{
             store,
+            apiKey:'77e03a919accfe25ba10020264bae0f0',
+            apiUri: 'https://api.themoviedb.org/3/search/movie'
         }
     },
 
     methods: {
-        
+        getMovies(){
+            //chiamata API
+        }
     },
 
     created(){
-
+        this.getMovies();
     },
 }
 </script>
 
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <header>
+        <label for="use-searchbar">
+            insert your query:
+        </label>
+        <input type="text" id="use-searchbar">
+        <button>Search</button>
+    </header>
 </template>
 
 <style lang="scss">
-    
+    button{
+        margin-left: 1rem;
+    }
 </style>
